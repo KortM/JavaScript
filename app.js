5f9e19aa91e38e967d2f5507a4ebca99f2358a28
kort <cort202@gmail.com>
2025-03-22 14:10:22 +0300

    Add testing

--- app.js
+++ app.js
@@ -15,3 +15,24 @@ let hourToExitDays = (11 - 2);
 let priceHour = 80; 
 console.log('Успею ли выполнить работу?: ', amountWorkDays <= hourToExitDays);
 console.log('Сколько за это запрошу?: ', priceHour * (amountWorkDays * 5) + '$');
+
+//Шаблонные строки
+const projectName = 'Сайт магазина';
+const price = 2000; 
+const author = 'Василий Пупкин';
+
+const template = author + ' заказал ' + projectName + ' по цене ' + price;
+
+const template2 = `${author} заказал ${projectName} по цене ${price}$`;
+console.log(template2);
+
+const template3 = 'Проект \n' + 'Цена: '+ price + '$';
+console.log(template3);
+
+const template4 = `Проект 
+Цена: ${price}$`
+console.log(template4);
+
+//Преобразование типов
+const age = '18';
+console.log(Number(age) + 5);

e05df7a93dd414509efcacdb3fdf1c443368f609
kort <cort202@gmail.com>
2025-03-24 21:14:17 +0300

    Ternal operators

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-const role = 'manager';
-
-if (role == 'manager'){
-    console.log('Менеджер');
-}else if (role == 'admin'){
-    console.log('Админ');
-}else if (role == 'ceo'){
-    console.log('CEO');
-}else{
-    console.log('I dont know you');
-}
-
-switch (role){
-    case 'manager':
-        console.log('Менеджер');
-        break;
-    case 'admin':
-        console.log('Админ');
-        break;
-    default:
-        console.log('I don`t know you');
-}
+//Тернарные операторы
+let val = prompt("Сколько будет 7 + 15");
+console.log(
+  `${val == 22 ? "Успех" : val == "Я не робот" ? "Успех" : "Вы робот!"}`
+);
